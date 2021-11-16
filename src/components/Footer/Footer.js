9d10619804b4e4548ef09a424d2513f96773a71b
Bruno Barbosa <brb.dev@outlook.com>
2021-11-16 19:47:55 -0400

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,8 +15,7 @@ export default class Footer extends Component {
                                 </svg>
                             </a>
                         </li>
-                    </ul>
-                    <ul>
+                    
                         <li>
                             <a href="https://www.github.com/ALTbruno" target="_blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#E9EAEC" class="bi bi-github" viewBox="0 0 16 16">
@@ -24,8 +23,7 @@ export default class Footer extends Component {
                                 </svg>
                             </a>
                         </li>
-                    </ul>
-                    <ul>
+
                         <li>
                             <a href="mailto:brb.dev@outlook.com" target="_blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#E9EAEC" class="bi bi-envelope-fill" viewBox="0 0 16 16">
